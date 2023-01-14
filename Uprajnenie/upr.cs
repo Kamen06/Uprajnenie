a3c7d52e7e8bf3bda12e7c3c075a6892f170b320
Kamen06 <kammen06@gmail.com>
2023-01-14 12:05:16 +0200

    Add project files.

---
+++ Uprajnenie/upr.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Uprajnenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sum = int.Parse(Console.ReadLine());
+            //int num = int.Parse(Console.ReadLine());
+            //int a = 1;
+            //for (int I = 0; I <= 9; I++)
+            //{
+            //    for (int II = 0; II <= 9; II++)
+            //    {
+            //        for (int III = 0; III <= 9; III++)
+            //        {
+            //            for (char IV = 'a'; IV <= 'z'; IV++)
+            //            {
+            //                for (char V = 'a'; V <= 'z'; V++)
+            //                {
+            //                    for (int VI = 0; VI <= 9; VI++)
+            //                    {
+            //                        if (I + II + III + (char)IV + (char)V + VI == sum)
+            //                        {
+            //                            if (a <= num)
+            //                            {
+            //                                Console.Write($"{I}{II}{III}{IV}{V}{VI} ");
+            //                                a = a + 1;
+            //                            }
+            //                        }
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+
+
+            //////////₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿₿
+            //////////BITCOIN
+
+            //double pari = int.Parse(Console.ReadLine());
+            //double b_cena = int.Parse(Console.ReadLine());
+            //double satoshi = int.Parse(Console.ReadLine());
+
+            //double zakupen_b = pari / b_cena;
+            //double taksa = zakupen_b * (satoshi * 1024) / 100000000;
+
+            //double obsht_b = pari / b_cena - taksa;
+
+            //double pechalba = obsht_b / 10;
+
+            //double total_b = obsht_b - pechalba;
+            //Console.WriteLine("Total bitcoin after expenses: " + $"{total_b:f5}" + " BTC");
+            //Console.WriteLine("Tax paid: " + $"{taksa * b_cena:f2}" + " USD");
+            //Console.WriteLine("Programmer`s payment: " + $"{pechalba:f5}" + " BTC");
+
+
+        }
+    }
+}
